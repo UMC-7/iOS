ff2a8d35ead627fa64b1c0eafec692c3afcbd6f7
guaugua <evan24865@naver.com>
2024-10-01 15:44:00 +0900

    1주차 과제

---
+++ Practice/2nd_Week/2nd_Week/2nd_Week/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2nd_Week
+//
+//  Created by 이은찬 on 10/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UMC_KREAM/1st_Week/Models/LoginModel.swift
@@ -0,0 +1,14 @@
+//
+//  LoginModel.swift
+//  1st_Week
+//
+//  Created by 이은찬 on 9/30/24.
+//
+
+import Foundation
+
+struct LoginModel{
+    var ID: String = ""
+    
+    var PW: String = ""
+}
