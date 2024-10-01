955ad90036358a9ae4cb22ce9f176fe27fedcae0
yoonsijin <trsj1017@naver.com>
2024-10-01 21:08:52 +0900

    1주차 과제 최종수정

--- sireal/UMC_KREAM/UMC_KREAM/Views/LoginView.swift
+++ sireal/UMC_KREAM/UMC_KREAM/Views/LoginView.swift
@@ -3,12 +3,35 @@ import SnapKit
 
 class LoginView: UIView {
 
+    // MARK: - 초기화 구문
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        subviews()
+        constraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Properties
+    
     // 상단 로고
     private let headerLogoView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "Logo")
         return imageView
     }()
+    
+    
+    // 아이디 스택뷰
+    private lazy var idStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        return stackView
+    }()
+    
     
     // 아이디 라벨
     private let idLabel: UILabel = {
@@ -19,6 +42,38 @@ class LoginView: UIView {
         return label
     }()
     
+    // 아이디 입력 필드
+    private let idTextField: UITextField = {
+        let text = UITextField()
+        text.leftViewMode = .always
+        text.leftView  = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: 1))
+        text.font = UIFont.systemFont(ofSize: 25)
+        text.clipsToBounds = true
+        text.layer.cornerRadius = 15
+        text.layer.borderColor = UIColor.gray.cgColor
+        text.layer.borderWidth = 1
+        text.textColor = .gray
+        
+        // placeholder 텍스트 크기 설정
+        let placeholderText = "예) kream@kream.co.kr"
+        text.attributedPlaceholder = NSAttributedString(
+            string: placeholderText,
+            attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12)]
+        )
+        
+        return text
+    }()
+    
+    
+    // 비밀번호 스택뷰
+    private lazy var pwdStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        return stackView
+    }()
+    
+    
     // 비밀번호 라벨
     private let pwdLabel: UILabel = {
         let label = UILabel()
@@ -28,23 +83,28 @@ class LoginView: UIView {
         return label
     }()
     
-    // 아이디 입력 필드
-    private let idTextField: UITextField = {
-        let text = UITextField()
-        text.placeholder = "kream@kream.co.kr"
-        text.borderStyle = .roundedRect
-        text.textColor = .gray
-        return text
-    }()
-    
     // 비밀번호 입력 필드
     private let pwdTextField: UITextField = {
         let text = UITextField()
-        text.placeholder = "비밀번호를 입력해주세요"
-        text.borderStyle = .roundedRect
+        text.leftViewMode = .always
+        text.leftView  = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: 1))
+        text.font = UIFont.systemFont(ofSize: 25)
+        text.clipsToBounds = true
+        text.layer.cornerRadius = 15
+        text.layer.borderColor = UIColor.gray.cgColor
+        text.layer.borderWidth = 1
         text.textColor = .gray
+        
+        // placeholder 텍스트 크기 설정
+        let placeholderText = "비밀번호를 입력해주세요"
+        text.attributedPlaceholder = NSAttributedString(
+            string: placeholderText,
+            attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12)]
+        )
+        
         return text
     }()
+    
     
     // 로그인 버튼
     private let loginBtn: UIButton = {
@@ -57,51 +117,33 @@ class LoginView: UIView {
         return btn
     }()
     
-    // 카카오 로그인 버튼
-    private let kakaologinBtn: UIButton = {
-        let btn = UIButton()
-        var configuration = UIButton.Configuration.plain()
-        configuration.title = "카카오로 로그인"
-        configuration.baseBackgroundColor = .white
-        configuration.baseForegroundColor = .black
-        configuration.background.strokeColor = UIColor.gray
-        configuration.background.strokeWidth = 1
-        btn.configuration = configuration
-        return btn
+    // 카카오 & 로그인 버튼
+    private let loginButtonView: UIImageView = {
+        let buttonView = UIImageView()
+        buttonView.image = UIImage(named: "LoginButton")
+        return buttonView
     }()
     
-    // 애플 로그인 버튼
-    private let appleloginBtn: UIButton = {
-        let btn = UIButton()
-        var configuration = UIButton.Configuration.plain()
-        configuration.title = "Apple로 로그인"
-        configuration.baseBackgroundColor = .white
-        configuration.baseForegroundColor = .black
-        configuration.background.strokeColor = UIColor.gray
-        configuration.background.strokeWidth = 1
-        btn.configuration = configuration
-        return btn
-    }()
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        subviews()
-        constraints()
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    
     
     private func subviews() {
+        // 아이디 스택뷰
+        addSubview(idStackView)
+        idStackView.addArrangedSubview(idLabel)
+        idStackView.addArrangedSubview(idTextField)
+        
+        // 패스워드 스택뷰
+        addSubview(pwdStackView)
+        pwdStackView.addArrangedSubview(pwdLabel)
+        pwdStackView.addArrangedSubview(pwdTextField)
+        
+        // 아이디 비번 그룹 스택뷰
+        
+        
         addSubview(headerLogoView)
-        addSubview(idLabel)
-        addSubview(idTextField)
-        addSubview(pwdLabel)
-        addSubview(pwdTextField)
         addSubview(loginBtn)
-        addSubview(kakaologinBtn)
-        addSubview(appleloginBtn)
+        addSubview(loginButtonView)
+
     }
 
     private func constraints() {
@@ -112,29 +154,17 @@ class LoginView: UIView {
             $0.height.equalTo(75)
         }
         
-        idLabel.snp.makeConstraints {
+        idStackView.snp.makeConstraints {
             $0.top.equalTo(headerLogoView.snp.bottom).offset(87)
             $0.left.equalToSuperview().offset(45)
-        }
-        
-        idTextField.snp.makeConstraints {
-            $0.top.equalTo(idLabel.snp.bottom).offset(8)
+            $0.right.equalToSuperview().offset(-45)
+            }
+            
+        pwdStackView.snp.makeConstraints {
+            $0.top.equalTo(idStackView.snp.bottom).offset(17)
             $0.left.equalToSuperview().offset(45)
             $0.right.equalToSuperview().offset(-45)
-            $0.height.equalTo(34)
-        }
-        
-        pwdLabel.snp.makeConstraints {
-            $0.top.equalTo(idTextField.snp.bottom).offset(17)
-            $0.left.equalToSuperview().offset(45)
-        }
-        
-        pwdTextField.snp.makeConstraints {
-            $0.top.equalTo(pwdLabel.snp.bottom).offset(8)
-            $0.left.equalToSuperview().offset(45)
-            $0.right.equalToSuperview().offset(-45)
-            $0.height.equalTo(34)
-        }
+            }
         
         loginBtn.snp.makeConstraints {
             $0.top.equalTo(pwdTextField.snp.bottom).offset(17)
@@ -143,18 +173,12 @@ class LoginView: UIView {
             $0.height.equalTo(38)
         }
         
-        kakaologinBtn.snp.makeConstraints {
+        loginButtonView.snp.makeConstraints{
             $0.top.equalTo(loginBtn.snp.bottom).offset(87)
             $0.left.equalToSuperview().offset(47.5)
             $0.right.equalToSuperview().offset(-47.5)
-            $0.height.equalTo(40)
-        }
-        
-        appleloginBtn.snp.makeConstraints {
-            $0.top.equalTo(kakaologinBtn.snp.bottom).offset(22)
-            $0.left.equalToSuperview().offset(47.5)
-            $0.right.equalToSuperview().offset(-47.5)
-            $0.height.equalTo(40)
+            $0.width.equalTo(298)
+            $0.height.equalTo(102)
         }
     }
 }
